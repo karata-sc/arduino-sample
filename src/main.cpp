5c34969e8383fd81e4e94b1b56d421bf781e1a94
karata-sc <ss78gmad@gmail.com>
2020-11-28 09:49:26 +0000

    Add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+#define LED_BUILTIN 13
+// オンボードのLEDを点滅
+void setup()
+{
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop()
+{
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(500);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(500);
+}
